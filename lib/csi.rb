73e571df49c49debdb6802abc0ccc3af25aa5869
Lee Reilly <lee@leereilly.net>
2013-02-20 21:23:41 -0800

    Minor refactor on the :train:

--- lib/csi.rb
+++ lib/csi.rb
@@ -1,27 +1,35 @@
 class CSI
-  def self.lookup_sic(code)
-    return if code.nil?
 
-    begin
-      file = File.open("#{File.expand_path(__FILE__+'/..')}/data/sic/2000/#{code}", "rb")
-      contents = file.read
-      contents.strip!
-      return contents
-    rescue
-      return nil
-    end
+  CURRENT_SIC_YEAR   = 2000 # last accurate data set for SIC codes
+  CURRENT_NAICS_YEAR = 2012 # last accurate data set for NAICS codes
+
+  # Lookup the SIC code.
+  #
+  # Returns a String value of the SIC classification, or nil if not found.
+  def self.lookup_sic(code)
+    lookup_code("sic", CURRENT_SIC_YEAR, code)
   end
 
+  # Lookup the NAICS code.
+  #
+  # Returns a String value of the NAICS classification, or nil if not found.
   def self.lookup_naics(code)
-    return nil if code.nil?
+    lookup_code("naics", CURRENT_NAICS_YEAR, code)
+  end
 
+  private
+
+  # Lookup the classification based on the type, year, and code.
+  #
+  # Returns a String value for the resquested classification, or nil if not
+  # found.
+  def self.lookup_code(type, year, code)
     begin
-      file = File.open("#{File.expand_path(__FILE__+'/..')}/data/naics/2012/#{code}", "rb")
+      file = File.open("#{File.expand_path(__FILE__+'/..')}/data/#{type}/#{year}/#{code}", "rb")
       contents = file.read
       contents.strip!
-      return contents
     rescue
-      return nil
+      nil
     end
   end
 end
