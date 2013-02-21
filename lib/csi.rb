efd4b924b49d4d8f6b584c12c90bfc411b0b3187
Lee Reilly <lee@leereilly.net>
2013-02-20 21:13:11 -0800

    Support looking up SIC codes

--- lib/csi.rb
+++ lib/csi.rb
@@ -1,6 +1,15 @@
 class CSI
   def self.lookup_sic(code)
     return if code.nil?
+
+    begin
+      file = File.open("#{File.expand_path(__FILE__+'/..')}/data/sic/2000/#{code}", "rb")
+      contents = file.read
+      contents.strip!
+      return contents
+    rescue
+      return nil
+    end
   end
 
   def self.lookup_naics(code)
--- test/test_csi.rb
+++ test/test_csi.rb
@@ -5,11 +5,23 @@ class TestCsi < Test::Unit::TestCase
     assert_equal CSI::lookup_naics(1123), 'Poultry and Egg Production'
   end
 
-  should "non find a NAICS classification for 0000" do
+  should "not find a NAICS classification for 0000" do
     assert_equal CSI::lookup_naics(0000), nil
   end
 
   should "not find a NAICS classification for nil" do
     assert_equal CSI::lookup_naics(nil), nil
+  end
+
+  should "find a SIC classification for 8041" do
+    assert_equal CSI::lookup_sic(8041), 'Offices And Clinics Of Chiropractors.'
+  end
+
+  should "not find a SIC classification for 0000" do
+    assert_equal CSI::lookup_sic(0000), nil
+  end
+
+  should "not find a SIC classification for nil" do
+    assert_equal CSI::lookup_sic(nil), nil
   end
 end
