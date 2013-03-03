359ea1a868458d559d6c2d6ce1f119b022fe19c0
Lee Reilly <lee@leereilly.net>
2013-03-02 21:27:21 -0800

    Don't support previous years' data

--- lib/csi.rb
+++ lib/csi.rb
@@ -1,22 +1,18 @@
 require 'toml'
 
 class CSI
-
-  CURRENT_SIC_YEAR   = 2000 # last accurate data set for SIC codes
-  CURRENT_NAICS_YEAR = 2012 # last accurate data set for NAICS codes
-
   # Lookup the SIC code.
   #
   # Returns a String value of the SIC classification, or nil if not found.
   def self.lookup_sic(code)
-    lookup_code("sic", CURRENT_SIC_YEAR, code)
+    lookup_code("sic", code)
   end
 
   # Lookup the NAICS code.
   #
   # Returns a String value of the NAICS classification, or nil if not found.
   def self.lookup_naics(code)
-    lookup_code("naics", CURRENT_NAICS_YEAR, code)
+    lookup_code("naics", code)
   end
 
   private
@@ -25,11 +21,11 @@ class CSI
   #
   # Returns a String value for the resquested classification, or nil if not
   # found.
-  def self.lookup_code(type, year, code)
+  def self.lookup_code(type, code)
     raise TypeError, 'Integer required' unless code.is_a? Integer
 
     begin
-      TOML.load_file("#{File.expand_path(__FILE__+'/..')}/data/#{type}/#{year}/#{code.to_i}.toml")["name"]
+      TOML.load_file("#{File.expand_path(__FILE__+'/..')}/data/#{type}/#{code}.toml")["name"]
     rescue
       return nil
     end
